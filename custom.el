4f275b2180d498f505bacc8245c12db936a1456a
Cristiano Carvalho <cristiano.carvalho23@icloud.com>
2023-03-03 23:44:38 -0300

    initial commit

---
+++ custom.el
@@ -0,0 +1,55 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(ansi-color-names-vector
+   ["#1f2430" "#f28779" "#bae67e" "#ffd580" "#73d0ff" "#d4bfff" "#5ccfe6" "#cbccc6"])
+ '(custom-safe-themes
+   '("84f6d5b890c59522d765c5610ff75b38d1716683606116cf9354b5fac4c4c35d" "776c1ab52648f98893a2aa35af2afc43b8c11dd3194a052e0b2502acca02bfce" "e7820b899036ae7e966dcaaec29fd6b87aef253748b7de09e74fdc54407a7a02" "c2d6d17e0918b7f3c73dc456db1fedfb86b16bf17ef9a926b6b891ea39098718" "6df412e59dbfe7f72f24319b9ee4513e40bb0e44384fc93a2c77399e641348f6" "ba9c91bc43996f2fa710e4b5145d9de231150103e142acdcf24adcaaf0db7a17" "680f62b751481cc5b5b44aeab824e5683cf13792c006aeba1c25ce2d89826426" "631c52620e2953e744f2b56d102eae503017047fb43d65ce028e88ef5846ea3b" "2dd4951e967990396142ec54d376cced3f135810b2b69920e77103e0bcedfba9" "7a424478cb77a96af2c0f50cfb4e2a88647b3ccca225f8c650ed45b7f50d9525" "6945dadc749ac5cbd47012cad836f92aea9ebec9f504d32fe89a956260773ca4" "944d52450c57b7cbba08f9b3d08095eb7a5541b0ecfb3a0a9ecd4a18f3c28948" default))
+ '(exwm-floating-border-color "#11131a")
+ '(fci-rule-color "#484f5b")
+ '(highlight-tail-colors
+   ((("#2e3737" "green" "green")
+     . 0)
+    (("#253542" "cyan" "blue")
+     . 20)))
+ '(jdee-db-active-breakpoint-face-colors (cons "#3d434d" "#ffcc66"))
+ '(jdee-db-requested-breakpoint-face-colors (cons "#3d434d" "#bae67e"))
+ '(jdee-db-spec-breakpoint-face-colors (cons "#3d434d" "#171b24"))
+ '(objed-cursor-color "#f28779")
+ '(package-selected-packages
+   '(autumn-light-theme ubuntu-theme nano-theme vs-light-theme jazz-theme solo-jazz-theme git-lens github-theme highlight-numbers vscode-dark-plus-theme vscode-icon lsp-treemacs flycheck-dialyxir lsp-ui flycheck-elixir flycheck elixir-mode vdiff-magit vdiff))
+ '(pdf-view-midnight-colors (cons "#cbccc6" "#1f2430"))
+ '(rustic-ansi-faces
+   ["#1f2430" "#f28779" "#bae67e" "#ffd580" "#73d0ff" "#d4bfff" "#5ccfe6" "#cbccc6"])
+ '(vc-annotate-background "#1f2430")
+ '(vc-annotate-color-map
+   (list
+    (cons 20 "#bae67e")
+    (cons 40 "#d1e07e")
+    (cons 60 "#e8da7f")
+    (cons 80 "#ffd580")
+    (cons 100 "#ffc573")
+    (cons 120 "#ffb666")
+    (cons 140 "#ffa759")
+    (cons 160 "#f0af90")
+    (cons 180 "#e2b7c7")
+    (cons 200 "#d4bfff")
+    (cons 220 "#deacd2")
+    (cons 240 "#e899a5")
+    (cons 260 "#f28779")
+    (cons 280 "#bb6c63")
+    (cons 300 "#84514e")
+    (cons 320 "#4d3639")
+    (cons 340 "#484f5b")
+    (cons 360 "#484f5b")))
+ '(vc-annotate-very-old-color nil)
+ '(widget-image-enable nil)
+ '(x-underline-at-descent-line t))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
