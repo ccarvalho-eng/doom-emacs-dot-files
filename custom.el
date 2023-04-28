bb1b35794b5b74c70c712cfc53cbb2f6940d96d3
Cristiano Carvalho <cristiano.carvalho23@icloud.com>
2023-04-27 23:01:23 -0300

    installed some themes

--- custom.el
+++ custom.el
@@ -5,24 +5,52 @@
  ;; If there is more than one, they won't work right.
  '(ansi-color-names-vector
    ["#1f2430" "#f28779" "#bae67e" "#ffd580" "#73d0ff" "#d4bfff" "#5ccfe6" "#cbccc6"])
+ '(chart-face-color-list
+   '("#b52c2c" "#4fd100" "#f1e00a" "#2fafef" "#bf94fe" "#47dfea" "#702020" "#007800" "#b08600" "#1f2f8f" "#5f509f" "#00808f"))
  '(custom-safe-themes
-   '("5f128efd37c6a87cd4ad8e8b7f2afaba425425524a68133ac0efd87291d05874" "a138ec18a6b926ea9d66e61aac28f5ce99739cf38566876dc31e29ec8757f6e2" "a44e2d1636a0114c5e407a748841f6723ed442dc3a0ed086542dc71b92a87aee" "2e05569868dc11a52b08926b4c1a27da77580daa9321773d92822f7a639956ce" "467dc6fdebcf92f4d3e2a2016145ba15841987c71fbe675dcfe34ac47ffb9195" "8d3ef5ff6273f2a552152c7febc40eabca26bae05bd12bc85062e2dc224cde9a" "70b596389eac21ab7f6f7eb1cf60f8e60ad7c34ead1f0244a577b1810e87e58c" "51c71bb27bdab69b505d9bf71c99864051b37ac3de531d91fdad1598ad247138" "a589c43f8dd8761075a2d6b8d069fc985660e731ae26f6eddef7068fece8a414" "991ca4dbb23cab4f45c1463c187ac80de9e6a718edc8640003892a2523cb6259" "683b3fe1689da78a4e64d3ddfce90f2c19eb2d8ab1bab1738a63d8263119c3f4" "bf948e3f55a8cd1f420373410911d0a50be5a04a8886cabe8d8e471ad8fdba8e" "eb122e1df607ee9364c2dfb118ae4715a49f1a9e070b9d2eb033f1cefd50a908" "633f8515a60037dfedd800f3c20275da4433cd26dbc05bbeae1859c963364df8" "7c20c453ad5413b110ccc3bb5df07d69999d741d29b1f894bd691f52b4abdd31" "84f6d5b890c59522d765c5610ff75b38d1716683606116cf9354b5fac4c4c35d" "776c1ab52648f98893a2aa35af2afc43b8c11dd3194a052e0b2502acca02bfce" "e7820b899036ae7e966dcaaec29fd6b87aef253748b7de09e74fdc54407a7a02" "c2d6d17e0918b7f3c73dc456db1fedfb86b16bf17ef9a926b6b891ea39098718" "6df412e59dbfe7f72f24319b9ee4513e40bb0e44384fc93a2c77399e641348f6" "ba9c91bc43996f2fa710e4b5145d9de231150103e142acdcf24adcaaf0db7a17" "680f62b751481cc5b5b44aeab824e5683cf13792c006aeba1c25ce2d89826426" "631c52620e2953e744f2b56d102eae503017047fb43d65ce028e88ef5846ea3b" "2dd4951e967990396142ec54d376cced3f135810b2b69920e77103e0bcedfba9" "7a424478cb77a96af2c0f50cfb4e2a88647b3ccca225f8c650ed45b7f50d9525" "6945dadc749ac5cbd47012cad836f92aea9ebec9f504d32fe89a956260773ca4" "944d52450c57b7cbba08f9b3d08095eb7a5541b0ecfb3a0a9ecd4a18f3c28948" default))
+   '("02f57ef0a20b7f61adce51445b68b2a7e832648ce2e7efb19d217b6454c1b644" "8f663b8939be3b54d70a4c963d5d0f1cfd278f447cb4257df6c4571fb8c71bca" "82225f1fa1e4d3b00c63700f691fc0dc7c9bdab8a996e6a78f451f9a15bd74fc" "59820d0ca7ba29984ab4d255f9fcfaa28041ad6ae4b6ae2ffb2ccfbfaec210e4" "171d1ae90e46978eb9c342be6658d937a83aaa45997b1d7af7657546cae5985b" "24b2a9551990485ee6762328a2eb964ba2afdc76d98c216d829b5ac1e51fd0e8" "098bc2b3038a9a58b2f7034262b54f56a547d8d9a09ebe5b7a4a5fb6fbcaeae5" "8feca8afd3492985094597385f6a36d1f62298d289827aaa0d8a62fe6889b33c" "1d78d6d05d98ad5b95205670fe6022d15dabf8d131fe087752cc55df03d88595" "c48551a5fb7b9fc019bf3f61ebf14cf7c9cdca79bcb2a4219195371c02268f11" "3cc2385c39257fed66238921602d8104d8fd6266ad88a006d0a4325336f5ee02" "3cd28471e80be3bd2657ca3f03fbb2884ab669662271794360866ab60b6cb6e6" "96998f6f11ef9f551b427b8853d947a7857ea5a578c75aa9c4e7c73fe04d10b4" "e9776d12e4ccb722a2a732c6e80423331bcb93f02e089ba2a4b02e85de1cf00e" "72a81c54c97b9e5efcc3ea214382615649ebb539cb4f2fe3a46cd12af72c7607" "58c6711a3b568437bab07a30385d34aacf64156cc5137ea20e799984f4227265" "3d5ef3d7ed58c9ad321f05360ad8a6b24585b9c49abcee67bdcbb0fe583a6950" "b3775ba758e7d31f3bb849e7c9e48ff60929a792961a2d536edec8f68c671ca5" "9b59e147dbbde5e638ea1cde5ec0a358d5f269d27bd2b893a0947c4a867e14c1" "d5db948d648857a3692c3237fd9600f7460a4e5f57a70e48ad1ab77dc991c657" "0d54c78d969fc00808de394689356fc91356fe3896bc20227f9572a2ad63c8eb" "67f02f75c5813133fc9b2e3fad000231e9c94a3cdc731fe01fb1d9b082606570" "8dcb2490e383506653f10ebb50cc2fec1c29c850070e277525f89e6a4f1f14d8" "979525ee3cdbe0d4ceab63147ec710be4cbdac0e66aae9d280c05bcbff89b15d" "fef1ae76cbc3d5fe957160406cf034e5a352037eb5a7ace339fcddc26ada5f9f" "33876ef6caf9c4cb35f8bc9430fb64ba503036a4e80532bb764edec121900248" "2a7d6f6a46b1b8504b269029a7375597f208a5cdfa1ea125b09255a592bb326e" "544bb10f6c6d7338be3bc73d147f26273d62e094e7643bfa842dffa9d742e30a" "306d853c5b47e1baf5e815eb49daa8a46d7f54d3f5ab624f3b30a6c1eb8e1f0c" "72ca2b0770981be70839fc2b738fc81f6268477653a6d89df67f0f94eb64e1e7" "ffef467dfed832df46d4e188049e52aad1d64c16070484fc6b62f158ece95471" "f48be80177f0d9a2b19d8dc19f3903d9be3c4d885d110e82b591d1184586fad0" "46c65f6d9031e2f55b919b1486952cddcc8e3ee081ade7eb2ffb6a68a804d30e" "74367676a7b0562975704f8e576d5e103451527b36c9226a013cd8f3ae2140f5" "dbc6d947d551aa03090daf6256233454c6a63240e17a8f3d77889d76fef1749d" "0af4fc8329f73bd771df30015858813385461513e044df730fc805a49f5ece52" "e16cd3e6093cf35e65dbac6c7649931936f32f56be34477cb7cbe1ee332c5b99" "cc626529cf94f8fd259ebd67e7e85af48b080fd8055f70cd7c4f1c0c86c91179" "84c2c93ce268699838b51eeeaaec46e064d5048e232b39113b73209be3ef6fd4" "c2efe6f5e2bd0bddfb2d6e26040545768939d2029f77e6b6a18d1ee0e0cb1033" "72cc2c6c5642b117034b99dcc3a33ff97a66593429c7f44cd21b995b17eebd4e" "f04c8886994146dcc6285e28cdb400e3f6909157e9f217871f4962d4ad262286" "3a78eb7a46504ff103b463f060b3cefe562f2b48f56b79c22102990f0c3f6aab" "f053f92735d6d238461da8512b9c071a5ce3b9d972501f7a5e6682a90bf29725" "5f128efd37c6a87cd4ad8e8b7f2afaba425425524a68133ac0efd87291d05874" "a138ec18a6b926ea9d66e61aac28f5ce99739cf38566876dc31e29ec8757f6e2" "a44e2d1636a0114c5e407a748841f6723ed442dc3a0ed086542dc71b92a87aee" "2e05569868dc11a52b08926b4c1a27da77580daa9321773d92822f7a639956ce" "467dc6fdebcf92f4d3e2a2016145ba15841987c71fbe675dcfe34ac47ffb9195" "8d3ef5ff6273f2a552152c7febc40eabca26bae05bd12bc85062e2dc224cde9a" "70b596389eac21ab7f6f7eb1cf60f8e60ad7c34ead1f0244a577b1810e87e58c" "51c71bb27bdab69b505d9bf71c99864051b37ac3de531d91fdad1598ad247138" "a589c43f8dd8761075a2d6b8d069fc985660e731ae26f6eddef7068fece8a414" "991ca4dbb23cab4f45c1463c187ac80de9e6a718edc8640003892a2523cb6259" "683b3fe1689da78a4e64d3ddfce90f2c19eb2d8ab1bab1738a63d8263119c3f4" "bf948e3f55a8cd1f420373410911d0a50be5a04a8886cabe8d8e471ad8fdba8e" "eb122e1df607ee9364c2dfb118ae4715a49f1a9e070b9d2eb033f1cefd50a908" "633f8515a60037dfedd800f3c20275da4433cd26dbc05bbeae1859c963364df8" "7c20c453ad5413b110ccc3bb5df07d69999d741d29b1f894bd691f52b4abdd31" "84f6d5b890c59522d765c5610ff75b38d1716683606116cf9354b5fac4c4c35d" "776c1ab52648f98893a2aa35af2afc43b8c11dd3194a052e0b2502acca02bfce" "e7820b899036ae7e966dcaaec29fd6b87aef253748b7de09e74fdc54407a7a02" "c2d6d17e0918b7f3c73dc456db1fedfb86b16bf17ef9a926b6b891ea39098718" "6df412e59dbfe7f72f24319b9ee4513e40bb0e44384fc93a2c77399e641348f6" "ba9c91bc43996f2fa710e4b5145d9de231150103e142acdcf24adcaaf0db7a17" "680f62b751481cc5b5b44aeab824e5683cf13792c006aeba1c25ce2d89826426" "631c52620e2953e744f2b56d102eae503017047fb43d65ce028e88ef5846ea3b" "2dd4951e967990396142ec54d376cced3f135810b2b69920e77103e0bcedfba9" "7a424478cb77a96af2c0f50cfb4e2a88647b3ccca225f8c650ed45b7f50d9525" "6945dadc749ac5cbd47012cad836f92aea9ebec9f504d32fe89a956260773ca4" "944d52450c57b7cbba08f9b3d08095eb7a5541b0ecfb3a0a9ecd4a18f3c28948" default))
  '(exwm-floating-border-color "#11131a")
  '(fci-rule-color "#484f5b")
+ '(flymake-error-bitmap
+   '(flymake-double-exclamation-mark standard-themes-fringe-error))
+ '(flymake-note-bitmap '(exclamation-mark standard-themes-fringe-info))
+ '(flymake-warning-bitmap '(exclamation-mark standard-themes-fringe-warning))
+ '(highlight-symbol-colors
+   '("#FFF68F" "#B7EB8F" "#76DDBA" "#91D5FF" "#ADC6FF" "#D3ADF7" "#FFADD2" "#FFA39E" "#FFD591"))
  '(highlight-tail-colors
    ((("#2e3737" "green" "green")
      . 0)
     (("#253542" "cyan" "blue")
      . 20)))
+ '(ibuffer-deletion-face 'standard-themes-mark-delete)
+ '(ibuffer-filter-group-name-face 'bold)
+ '(ibuffer-marked-face 'standard-themes-mark-select)
+ '(ibuffer-title-face 'default)
  '(jdee-db-active-breakpoint-face-colors (cons "#3d434d" "#ffcc66"))
  '(jdee-db-requested-breakpoint-face-colors (cons "#3d434d" "#bae67e"))
  '(jdee-db-spec-breakpoint-face-colors (cons "#3d434d" "#171b24"))
+ '(linum-format " %5i ")
+ '(nrepl-message-colors
+   '("#183691" "#969896" "#a71d5d" "#969896" "#0086b3" "#795da3" "#a71d5d" "#969896"))
  '(objed-cursor-color "#f28779")
+ '(org-src-block-faces
+   '(("emacs-lisp"
+      (:background "#F0FFF0"))
+     ("dot"
+      (:foreground "gray50"))))
  '(package-selected-packages
-   '(flatland-theme flatfluc-theme flatui-dark-theme web-mode flyspell-correct autumn-light-theme ubuntu-theme nano-theme vs-light-theme jazz-theme solo-jazz-theme git-lens github-theme highlight-numbers vscode-dark-plus-theme vscode-icon lsp-treemacs flycheck-dialyxir lsp-ui flycheck-elixir flycheck elixir-mode vdiff-magit vdiff))
+   '(standard-themes tangotango-theme timu-macos-theme atom-one-dark-theme organic-green-theme github-dark-vscode-theme github-modern-theme sublime-themes shanty-themes stimmung-themes sketch-themes ef-themes hydandata-light-theme zweilight-theme flatland-theme flatfluc-theme flatui-dark-theme web-mode flyspell-correct autumn-light-theme ubuntu-theme nano-theme vs-light-theme jazz-theme solo-jazz-theme git-lens github-theme highlight-numbers vscode-dark-plus-theme vscode-icon lsp-treemacs flycheck-dialyxir lsp-ui flycheck-elixir flycheck elixir-mode vdiff-magit vdiff))
  '(pdf-view-midnight-colors (cons "#cbccc6" "#1f2430"))
  '(rustic-ansi-faces
    ["#1f2430" "#f28779" "#bae67e" "#ffd580" "#73d0ff" "#d4bfff" "#5ccfe6" "#cbccc6"])
+ '(tetris-x-colors
+   [[229 192 123]
+    [97 175 239]
+    [209 154 102]
+    [224 108 117]
+    [152 195 121]
+    [198 120 221]
+    [86 182 194]])
  '(vc-annotate-background "#1f2430")
  '(vc-annotate-color-map
    (list
