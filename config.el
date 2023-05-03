88aa1a9b07a9b65b745ac50e19502c839226265d
Cristiano Carvalho <cristiano.carvalho23@icloud.com>
2023-05-03 19:39:35 -0300

    refactor: organize configs

--- config.el
+++ config.el
@@ -2,101 +2,107 @@
 
 ;; Place your private configuration here! Remember, you do not need to run 'doom
 ;; sync' after modifying this file!
-;;
-;;
 
-;; Setup ENV for MAC
-(when (memq window-system '(mac ns x))
- (exec-path-from-shell-initialize))
-
-;; Some functionality uses this to identify you, e.g. GPG configuration, email
-;; clients, file templates and snippets.
+;; -----------------------------------------------------------------------------
+;; User configuration
+;; -----------------------------------------------------------------------------
 (setq user-full-name "Cristiano Carvalho"
       user-mail-address "cristiano.dev@icloud.com"
-      projectile-project-search-path '("~/Projects")
-      )
+      projectile-project-search-path '("~/Projects"))
 
-;; Doom exposes five (optional) variables for controlling fonts in Doom. Here
-;; are the three important ones:
-;;
-;; + `doom-font'
-;; + `doom-variable-pitch-font'
-;; + `doom-big-font' -- used for `doom-big-font-mode'; use this for
-;;   presentations or streaming.
-;;
-;; They all accept either a font-spec, font string ("Input Mono-12"), or xlfd
-;; font string. You generally only need these two:
+;; -----------------------------------------------------------------------------
+;; Fonts
+;; -----------------------------------------------------------------------------
 (setq doom-font (font-spec :family "MonacoB" :size 10 :weight 'bold))
 
-;; Elixir LSP
-(after! lsp-clients
-  (lsp-register-client
-   (make-lsp-client :new-connection
-    (lsp-stdio-connection
-        (expand-file-name
-          "~/.elixir-ls/release/language_server.sh"))
-        :major-modes '(elixir-mode)
-        :priority -1
-        :server-id 'elixir-ls
-        :initialized-fn (lambda (workspace)
-            (with-lsp-workspace workspace
-             (let ((config `(:elixirLS
-                             (:mixEnv "dev"
-                                     :dialyzerEnabled
-                                     :json-false))))
-             (lsp--set-configuration config)))))))
+;; -----------------------------------------------------------------------------
+;; Theme
+;; -----------------------------------------------------------------------------
+(setq doom-theme 'doom-one)
 
-;; Configure LSP-ui to define when and how to display informations.
-(after! lsp-ui
-  (setq lsp-ui-doc-max-height 20
-        lsp-ui-doc-max-width 80
-        lsp-ui-sideline-ignore-duplicate t
-        lsp-ui-doc-header t
-        lsp-ui-doc-include-signature t
-        lsp-ui-doc-position 'bottom
-        lsp-ui-doc-use-webkit nil
-        lsp-ui-flycheck-enable t
-        lsp-ui-imenu-kind-position 'left
-        lsp-ui-sideline-code-actions-prefix "💡"
-        ;; fix for completing candidates not showing after “Enum.”:
-        company-lsp-match-candidate-predicate #'company-lsp-match-candidate-prefix
-        ))
-
-;; There are two ways to load a theme. Both assume the theme is installed and
-;; available. You can either set `doom-theme' or manually load a theme with the
-;; `load-theme' function. This is the default:
-;; (setq doom-theme 'solo-jazz)
-
-;; If you use `org' and don't want your org files in the default location below,
-;; change `org-directory'. It must be set before org loads!
-(setq display-line-numbers-type nil)
+;; -----------------------------------------------------------------------------
+;; Org configuration
+;; -----------------------------------------------------------------------------
 (setq org-directory "~/org/")
 
-;; This determines the style of line numbers in effect. If set to `nil', line
-;; numbers are disabled. For relative line numbers, set this to `relative'.
+;; -----------------------------------------------------------------------------
+;; Line numbers
+;; -----------------------------------------------------------------------------
 (setq display-line-numbers-type t)
 
-;; Sort keys
-(setq which-key-sort-order 'which-key-key-order-alpha)
-
-;; (add-to-list 'default-frame-alist '(fullscreen . maximized))
+;; -----------------------------------------------------------------------------
+;; Frame configuration
+;; -----------------------------------------------------------------------------
 (add-to-list 'default-frame-alist '(width . 210))
 (add-to-list 'default-frame-alist '(height . 68))
 
+;; -----------------------------------------------------------------------------
+;; Keybindings
+;; -----------------------------------------------------------------------------
+(global-set-key (kbd "C-s") 'save-buffer)
+(global-set-key (kbd "s-s") 'save-buffer)
+
+;; -----------------------------------------------------------------------------
+;; macOS configuration
+;; -----------------------------------------------------------------------------
+(when (memq window-system '(mac ns x))
+  (exec-path-from-shell-initialize))
+
+;; -----------------------------------------------------------------------------
+;; Elixir configuration
+;; -----------------------------------------------------------------------------
+(use-package! exunit)
+
+;; -----------------------------------------------------------------------------
+;; LSP configuration
+;; -----------------------------------------------------------------------------
+(after! lsp-clients
+  ;; Elixir LSP
+  (lsp-register-client
+   (make-lsp-client :new-connection
+                    (lsp-stdio-connection
+                     (expand-file-name
+                      "~/.elixir-ls/release/language_server.sh"))
+                    :major-modes '(elixir-mode)
+                    :priority -1
+                    :server-id 'elixir-ls
+                    :initialized-fn (lambda (workspace)
+                                      (with-lsp-workspace workspace
+                                        (let ((config `(:elixirLS
+                                                        (:mixEnv "dev"
+                                                                 :dialyzerEnabled
+                                                                 :json-false))))
+                                          (lsp--set-configuration config)))))))
+  ;; LSP UI
+  (after! lsp-ui
+    (setq lsp-ui-doc-max-height 20
+          lsp-ui-doc-max-width 80
+          lsp-ui-sideline-ignore-duplicate t
+          lsp-ui-doc-header t
+          lsp-ui-doc-include-signature t
+          lsp-ui-doc-position 'bottom
+          lsp-ui-doc-use-webkit nil
+          lsp-ui-flycheck-enable t
+          lsp-ui-imenu-kind-position 'left
+          lsp-ui-sideline-code-actions-prefix "💡"
+          company-lsp-match-candidate-predicate #'company-lsp-match-candidate-prefix))
+
+;; -----------------------------------------------------------------------------
+;; Folding configuration
+;; -----------------------------------------------------------------------------
+(setq lsp-enable-folding t)
+(use-package! lsp-origami)
+(add-hook! 'lsp-after-open-hook #'lsp-origami-try-enable)
+
+;; -----------------------------------------------------------------------------
+;; Custom functions
+;; -----------------------------------------------------------------------------
 (defun elixir-append-inspect()
   (interactive)
   (evil-append-line nil)
   (insert " |> IO.inspect")
-  (evil-normal-state)
-)
+  (evil-normal-state))
 
-(global-set-key (kbd "C-s") 'save-buffer)
-(global-set-key (kbd "s-s") 'save-buffer)
-
-;; Configure exunit
-(use-package! exunit)
-
-;; Configure mix commands to run in the project root directory
 (defun my-elixir-mix-credo ()
   (interactive)
   (let ((default-directory (projectile-project-root)))
@@ -107,7 +113,9 @@
   (let ((default-directory (projectile-project-root)))
     (compile "mix dialyzer")))
 
-;; Setup some keybindings for exunit and lsp-ui
+;; -----------------------------------------------------------------------------
+;; Elixir keybindings
+;; -----------------------------------------------------------------------------
 (map! :mode elixir-mode
       :leader
       :desc "Sort Lines" :nve "l" #'sort-lines
@@ -117,6 +125,9 @@
       :desc "Mix Credo" :nve "mc" #'my-elixir-mix-credo
       :desc "Mix Dialyzer" :nve "md" #'my-elixir-mix-dialyzer)
 
+;; -----------------------------------------------------------------------------
+;; LSP mode file watch ignored
+;; -----------------------------------------------------------------------------
 (after! lsp-mode
   (dolist (match
            '("[/\\\\].direnv$"
@@ -127,35 +138,20 @@
              "[/\\\\]_build"))
     (add-to-list 'lsp-file-watch-ignored match)))
 
+;; -----------------------------------------------------------------------------
+;; Keychain refresh
+;; -----------------------------------------------------------------------------
 (keychain-refresh-environment)
 
-;; Here are some additional functions/macros that could help you configure Doom:
-;;
-;; - `load!' for loading external *.el files relative to this one
-;; - `use-package' for configuring packages
-;; - `after!' for running code after a package has loaded
-;; - `add-load-path!' for adding directories to the `load-path', relative to
-;;   this file. Emacs searches the `load-path' when you load packages with
-;;   `require' or `use-package'.
-;; - `map!' for binding new keys
-;;
-;; To get information about any of these functions/macros, move the cursor over
-;; the highlighted symbol at press 'K' (non-evil users must press 'C-c g k').
-;; This will open documentation for it, including demos of how they are used.
-;;
-;; You can also try 'gd' (or 'C-c g d') to jump to their definition and see how
-;; they are implemented.
-
+;; -----------------------------------------------------------------------------
+;; Neotree configuration
+;; -----------------------------------------------------------------------------
 (doom-themes-neotree-config)
 (setq doom-themes-neotree-file-icons t)
 
-;; Enable folding
-(setq lsp-enable-folding t)
-
-;; Add origami and LSP integration
-(use-package! lsp-origami)
-(add-hook! 'lsp-after-open-hook #'lsp-origami-try-enable)
-
+;; -----------------------------------------------------------------------------
+;; ChatGPT configuration
+;; -----------------------------------------------------------------------------
 (use-package! chatgpt
   :defer t
   :config
@@ -166,10 +162,12 @@
     :side 'bottom :size .5 :ttl nil :quit t :modeline nil)
   :bind ("C-c q" . chatgpt-query))
 
+;; -----------------------------------------------------------------------------
+;; ChatGPT format string map
+;; -----------------------------------------------------------------------------
 (setq chatgpt-query-format-string-map '(
                                         ;; ChatGPT.el defaults
                                         ("doc" . "Please write the documentation for the following function.\n\n%s")
                                         ("bug" . "There is a bug in the following function, please help me fix it.\n\n%s")
                                         ("understand" . "What does the following function do?\n\n%s")
                                         ("improve" . "Please improve the following code.\n\n%s")))
-
