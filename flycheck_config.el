20b143c77d92567e445886dc9d5c86b7c8fe03ba
Cristiano Carvalho <cristiano.carvalho23@icloud.com>
2023-12-13 08:51:09 -0300

    chore: store notes on icloud

--- flycheck_config.el
+++ flycheck_config.el
@@ -50,7 +50,7 @@
 ;; Org configuration
 ;; -----------------------------------------------------------------------------
 ;; Define a variable to store the directory path for the journal files
-(defvar my-journal-directory "~/Library/Mobile Documents/com~apple~CloudDocs/Documents/org/"
+(defvar my-journal-directory "~/Library/Mobile Documents/com~apple~CloudDocs/Documents/journal/")
   "The directory where I store my journal files.")
 
 ;; Set the date format for org-journal entries
