b699c40eb45299537192499e1d3d6989d09d6914
Cristiano Carvalho <cristiano.carvalho23@icloud.com>
2024-01-03 10:08:08 -0300

    chore: change github-string constant

--- themes/github-theme.el
+++ themes/github-theme.el
@@ -50,7 +50,7 @@
     ("github-html-tag"               . "#63a35c")
     ("github-keyword"                . "#a71d5d")
     ("github-selection"              . "#b0cde7")
-    ("github-string"                 . "#69A270")
+    ("github-string"                 . "#3A804F") ;; Goblin
     ("github-text"                   . "#333333")
     ("github-white"                  . "#F8F8F8"))
   "List of GitHub colors.
