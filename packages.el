f8b607029fe50e9efe87d117d5f705e8e74bdfe0
Cristiano Carvalho <cristiano.carvalho23@icloud.com>
2024-06-19 01:37:26 +0100

    chore: install org-mode-ui

--- packages.el
+++ packages.el
@@ -14,6 +14,7 @@
 (package! keychain-environment)
 (package! easy-jekyll)
 (package! hurl-mode :recipe (:host github :repo "jaszhe/hurl-mode" :files ("*.el")))
+(package! org-roam-ui)
 
 ;; To install a package directly from a remote git repo, you must specify a
 ;; `:recipe'. You'll find documentation on what `:recipe' accepts here:
